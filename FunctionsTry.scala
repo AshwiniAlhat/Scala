d3e1640214bf68b9a7c962353292ba457923a4fb
Ashwini.Alhat <Ashwini.Alhat@ideas.com>
2015-11-18 14:04:02 +0530

    Tried Function

---
+++ FunctionsTry.scala
@@ -0,0 +1,12 @@
+package helloworld
+
+object  FunctionsTry {
+  def square(x:Double):Double= {
+    return x*x
+    }
+  
+    def main(args:Array[String]){
+    println("Square is " + this.square(2));
+  }
+
+}
